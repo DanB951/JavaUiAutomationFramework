b9a5082af59c3da8540ee48f1e7950e33735f76c
DanB951 <dan.borcea.iurii@gmail.com>
2023-06-13 21:02:04 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,25 @@
 package com.opencart;
 
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DriverManager manager = DriverManager.getInstance();
+        WebDriver driver = manager.getDriver();
+
+        String currentWindowsName = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-firefox-driver/4.9.1");
+        driver.close();
+
+        driver.switchTo().window(currentWindowsName);
+        manager.getDriver().get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-edge-driver/4.9.1");
+        driver.quit();
+
+        System.out.println("The driver is closed");
     }
 }
