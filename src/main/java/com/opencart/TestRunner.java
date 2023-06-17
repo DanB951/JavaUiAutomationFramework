fde2212e71436329612aa3056b64ea6b656ed39d
DanB951 <dan.borcea.iurii@gmail.com>
2023-06-17 14:15:04 +0300

    Implemented the FakeDataManager and create the first scenario.

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,25 +1,72 @@
 package com.opencart;
 
+import com.opencart.managers.DataFakerManager;
 import com.opencart.managers.DriverManager;
+import com.opencart.managers.ScrollManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 
 public class TestRunner {
-    public static void main(String[] args) {
-        DriverManager manager = DriverManager.getInstance();
-        WebDriver driver = manager.getDriver();
+    public static <JavascriptExecutor> void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        driver.get("https://google.com/");
 
         String currentWindowsName = driver.getWindowHandle();
 
         driver.switchTo().newWindow(WindowType.TAB);
 
-        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-firefox-driver/4.9.1");
+        driver.get("https://andreisecuqa.host/");
+
+        WebElement accountIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
+        accountIcon.click();
+
+        WebElement registerButton = driver.findElement(By.xpath("//a[normalize-space()='Register']"));
+        registerButton.click();
+
+        String firstName = DataFakerManager.getRandomName();
+        System.out.println(("The generated first name is: " + firstName));
+
+        String lastName = DataFakerManager.getRandomName();
+        System.out.println("The generated last name is: " + lastName);
+
+        String randomEmail = DataFakerManager.getRandomEmail("dan","@yahoo.com");
+        System.out.println("The generated email is: " + randomEmail);
+
+        String password = DataFakerManager.getRandomPassword(8,20);
+        System.out.println("The generated password is: " + password);
+
+        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+        firstNameInput.sendKeys(firstName);
+
+        WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
+        lastNameInput.sendKeys(lastName);
+
+        WebElement emailInput = driver.findElement(By.id("input-email"));
+        emailInput.sendKeys(randomEmail);
+
+        WebElement passwordInput = driver.findElement(By.id("input-password"));
+        passwordInput.sendKeys(password);
+
+        WebElement privacyToggle = driver.findElement(By.cssSelector("input[value='1'][name='agree']"));
+        ScrollManager.scrollToElement(driver, privacyToggle);
+        privacyToggle.click();
+
+        WebElement continueButton = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
+        continueButton.click();
+
+        Thread.sleep(5000);
+
+        System.out.println(driver.getCurrentUrl());
+
         driver.close();
 
         driver.switchTo().window(currentWindowsName);
-        manager.getDriver().get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-edge-driver/4.9.1");
+        driver.get("https://andreisecuqa.host");
         driver.quit();
 
-        System.out.println("The driver is closed");
+        System.out.println("The execution was finished");
     }
 }
